49baf77ac7f3ad5bc85446122ade9efb3024a41d
Tatsiana Krauchanka <tats.krauchanka@gmail.com>
2023-12-09 16:58:27 +0300

    feat: handle scroll burger

--- coffee-house/src/common/index.js
+++ coffee-house/src/common/index.js
@@ -4,11 +4,18 @@ const headerMenuList = document.getElementsByClassName("header-menu-list")[0];
 burger.onclick = () => {
   burger.classList.toggle("active");
   headerMenuList.classList.toggle("active");
-};
+
+  if (burger.classList.contains("active")) {
+    document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+  } else {
+    document.getElementsByTagName("body")[0].style.overflowY = "auto";
+  }
+};git
 
 document.querySelectorAll("#menu *").forEach((item) => {
   item.onclick = () => {
     headerMenuList.classList.remove("active");
     burger.classList.remove("active");
+    document.getElementsByTagName("body")[0].style.overflowY = "auto";
   };
 });
