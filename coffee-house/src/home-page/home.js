0db56541a48f7311ee6f664439cff59770220226
Tatsiana Krauchanka <tats.krauchanka@gmail.com>
2023-12-11 16:35:49 +0300

    feat: change styles

--- coffee-house/src/home-page/home.js
+++ coffee-house/src/home-page/home.js
@@ -7,10 +7,8 @@ const arrowNextElement = document.getElementById("arrow-next");
 const bars = document.querySelectorAll(".line");
 let countSlider = 0;
 
+//click on arrowNext
 arrowNextElement.addEventListener("click", nextSlider);
-
-let start = 0;
-let end = 0;
 
 function nextSlider() {
   slidersList[countSlider].style.animation = "next1 0.5s linear forwards";
@@ -21,15 +19,10 @@ function nextSlider() {
   }
   slidersList[countSlider].style.animation = "next2 0.5s linear forwards";
 
-  if (start >= 1500) {
-    start = 0;
-    end = 0;
-  }
-  start = Date.now();
-
   handleBars();
 }
 
+//click on arrowBack
 arrowBackElement.addEventListener("click", prevSlider);
 
 function prevSlider() {
@@ -43,46 +36,83 @@ function prevSlider() {
   handleBars();
 }
 
+//set interval for autosliding
 function autoSliding() {
-  interval = setInterval(function () {
-    nextSlider();
-    handleBars();
-  }, 1500);
-  /* 
   interval = setInterval(timer, 1500);
 
   function timer() {
     nextSlider();
     handleBars();
-  } */
+  }
 }
 
 autoSliding();
 
-let diff = 0;
-slidersList.forEach((slide) => {
-  slide.addEventListener("mouseover", function (e) {
-    e.preventDefault();
-    end = Date.now();
-    let diff = end - start;
-    console.log(diff);
-    clearInterval(interval);
-    document.querySelector(".line.active").style.animationPlayState = "paused";
+//hover effect - stop animation
+function handleAnimation() {
+  slidersList.forEach((slide) => {
+    let leftTimeAnimation = 0;
+    let fulledBarArea = 0;
+
+    slide.addEventListener("mouseover", function (e) {
+      e.stopPropagation();
+      clearInterval(interval);
+
+      /* let list = e.currentTarget.parentNode.nextElementSibling.children;
+          let activeBar = null;
+          Array.from(list).forEach((item) => {
+            if (item.children[0].classList.contains("active")) {
+              activeBar = item.children[0];
+            }
+          });
+          let styles = getComputedStyle(activeBar);
+          let activeBarWidth = parseInt(styles.width);
+      
+          fulledBarArea = (activeBarWidth * 100) / 40;
+          let leftFullBarArea = 100 - fulledBarArea;
+      
+          leftTimeAnimation = (leftFullBarArea * 1500) / 100;
+       */
+      document.querySelector(".line.active").style.animationPlayState =
+        "paused";
+    });
+
+    /* const removeBars = `
+      @keyframes removeBars {
+          from {
+            width: ${fulledBarArea}%;
+          }
+          to {
+            width: 100%;
+          }
+        }
+      `; */
+
+    slide.addEventListener("mouseout", function () {
+      document.querySelector(".line.active").style.animationPlayState =
+        "running";
+      /*  document.querySelector(
+            ".line.active"
+          ).style.animation = `removeBars ${450}ms linear infinite `; */
+      /*  document
+            .querySelector(".line.active")
+            .animate([
+              { width: `${fulledBarArea}`},
+              { width: "100%" },
+            ],
+            {
+              duration: leftTimeAnimation,
+              iterations: Infinity
+            }); */
+    });
+
+    slide.addEventListener("mouseout", autoSliding);
   });
+}
 
-  slide.addEventListener("mouseout", function () {
-    /*  console.log(diff); */
-    document.querySelector(".line.active").style.animationPlayState = "running";
-    document.querySelector(".line.active").style.animation = `moveBars ${
-      1500 - diff
-    }ms linear infinite forwards`;
+handleAnimation();
 
-    start = 0;
-  });
-
-  slide.addEventListener("mouseout", autoSliding);
-});
-
+//handle active/inactive bar
 function handleBars() {
   for (let i = 0; i < bars.length; i++) {
     bars[i].classList.remove("active");
@@ -90,6 +120,7 @@ function handleBars() {
   bars[countSlider].classList.add("active");
 }
 
+//switch sliders with bars
 function switchSlider() {
   for (let i = 0; i < bars.length; i++) {
     bars[i].onclick = (e) => {
