5413e22fdc56fce1be775ad1bfa6aab3be34490e
Tatiana <88846032+TatsianaKrav@users.noreply.github.com>
2024-03-09 10:50:22 +0300

    Chore: RSS-PZ-00 Setup configs (#30)
    * chore: init npm
    
    * chore: init tsc
    
    * chore: add prettier
    
    * chore: add eslint
    
    * chore: congif eslint
    
    * chore: add husky
    
    * chore: configure validate banch name
    
    * chore: configure husky
    
    * refactor: change eslint and husky configs
    
    * chore: add webpack
    
    * chore: enable source maps
    
    * refactor: change eslint configs
    
    * refactor: change webpack and eslint configs
    
    * refactor: modify eslint configs
    
    * refactor: update configs
    
    * feat: add ts file

---
+++ rss-puzzle/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs,ts}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    'react/jsx-filename-extension': [0],
+  },
+};
---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: './src/index.html' }],
+    }),
+    new EslingPlugin({ extensions: 'ts' }),
+  ],
+};
