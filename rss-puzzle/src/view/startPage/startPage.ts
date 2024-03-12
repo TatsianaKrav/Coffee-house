6f22728d543d5eb369149ed36ea94a0a8182a21e
Tatiana <88846032+TatsianaKrav@users.noreply.github.com>
2024-03-12 12:59:44 +0300

    feat: add name, description and bg to start page (#38)

--- rss-puzzle/src/view/startPage/startPage.ts
+++ rss-puzzle/src/view/startPage/startPage.ts
@@ -1,7 +1,9 @@
 import { IElementParams } from '../../interfaces/IElementParams';
 import Router from '../../router/router';
 import State from '../../state/state';
+import ElementCreator from '../../util/element-creator';
 import View from '../view';
+import './startPage.css';
 
 export default class StartPage extends View {
   state: State;
@@ -14,5 +16,30 @@ export default class StartPage extends View {
 
     super(params);
     this.state = state;
+
+    this.configureView();
+  }
+
+  configureView(): void {
+    const nameParams: IElementParams = {
+      tag: 'div',
+      cssClasses: ['game-name'],
+      textContent: 'English Word Puzzle',
+    };
+
+    const gameNameCreator: ElementCreator = new ElementCreator(nameParams);
+    this.elementCreator.addInnerElement(gameNameCreator.getElement());
+
+    const descriptionParams: IElementParams = {
+      tag: 'div',
+      cssClasses: ['description-game'],
+      textContent:
+        'Choose level and round, click on words and collect the right phrase in English. Use tooltips and see the result. At the end you will see an amazing image.',
+    };
+
+    const descriptionCreator: ElementCreator = new ElementCreator(
+      descriptionParams,
+    );
+    this.elementCreator.addInnerElement(descriptionCreator.getElement());
   }
 }
